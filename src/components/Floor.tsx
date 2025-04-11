8b7e488c62307182bba84a4feb18b40078ff9319
Park Seungjae <andrewpark0619@gmail.com>
2025-04-11 23:05:05 +0900

    feat(floor): 바닥 구현

---
+++ src/components/Floor.tsx
@@ -0,0 +1,13 @@
+import { useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { useMemo } from "react";
+import * as THREE from "three";
+
+export function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+      <planeGeometry args={[50, 50]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+}
