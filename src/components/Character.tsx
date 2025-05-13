e08b16c3cc1d9aa63f5e221db4fe269b8c02ebab
Park Seungjae <andrewpark0619@gmail.com>
2025-05-13 22:18:30 +0900

    feat/ 카메라 동작 파일 추가 및 app.tsx 코드 정리

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 import { Group } from "three";
 import { useAnimations } from "@react-three/drei";
 
-export default function GLTFCharacter(props: string) {
+export default function Character(props: string) {
   const gltf = useLoader(GLTFLoader, "/models/chung-walk.glb");
   const group = useRef<Group>(null);
   const { actions } = useAnimations(gltf.animations, group);
